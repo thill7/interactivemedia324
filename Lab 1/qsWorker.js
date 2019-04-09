cf54f684819ad61c5c22cf894e888318824283d7
thill7 <43824102+thill7@users.noreply.github.com>
2019-04-08 18:00:17 -0700

    Add files via upload

--- Lab 1/qsWorker.js
+++ Lab 1/qsWorker.js
@@ -1,5 +1,4 @@
 var swaps = 0;
-var size = 30;
 
 function avgArray(arr) {
 	var total = 0;
@@ -10,7 +9,7 @@ function avgArray(arr) {
 	return total;
 }
 
-async function testSort(testAmt) {
+function testSort(testAmt,size) {
 	var nums = [];
 	var swapsGroup = [];
 	var timeGroup = [];
@@ -26,8 +25,8 @@ async function testSort(testAmt) {
 			}
 		}
 		time = performance.now();
-		await quickSort(nums);
-		time = performance.now();
+		quickSort(nums);
+		time = performance.now() - time;
 		timeGroup.push(time);
 		swapsGroup.push(swaps);
 	}
@@ -40,14 +39,6 @@ async function testSort(testAmt) {
 	};
 	postMessage(message);
 }
-/*function getRandomColor() {
-	var letters = '0123456789ABCDEF';
-	var color = '#';
-	for (var i = 0; i < 6; i++) {
-	  color += letters[Math.floor(Math.random() * 16)];
-	}
-	return color;
-}*/
 
 async function wait(ms) {
   return new Promise(resolve => {
@@ -59,23 +50,10 @@ async function swap(nums,firstNum,secondNum) {
 	let temp = nums[firstNum];
 	nums[firstNum] = nums[secondNum];
 	nums[secondNum] = temp;
-	/*$("#sorted > div").each((i, cont) => {
-		$(cont).html(nums[i]);
-	});*/
-	await wait(2000);
 	swaps++;
-	//$("#swapsAmt").html(swaps);
 }
 var colors = [];
-async function quickSort(nums,start,end) {
-	/*var randColor = getRandomColor();
-	while(colors.includes(randColor)) {
-		randColor = getRandomColor();
-	}
-	colors.push(randColor);
-	for(let i = start; i < end + 1; i++) {
-		$("#sorted").children().eq(i).css({"background-color":randColor,"color":"ghostwhite"});
-	}*/
+function quickSort(nums,start,end) {
 	if(end - start > 0) {
 		var pivot = start;
 		var too_big_index = start + 1;
@@ -97,29 +75,6 @@ async function quickSort(nums,start,end) {
 	}
 }
 
-//nums = [40,20,10,80,60,50,7,30,100];
-/*nums = [];
-while(nums.length < size) {
-	let num = Math.floor(Math.random() * size) + 1;
-	if(!(nums.includes(num))) {
-		nums.push(num);
-	}
-}
-console.log(nums);
-$.each(nums, (i, num) => {
-	$("<div>").html(num).appendTo("#unsorted");
-});
-$.each(nums, (i, num) => {
-	$("<div>").html(num).appendTo("#sorted");
-});
-
-var start = window.performance.now();
-quickSort(nums,0,(nums.length - 1));
-start = window.performance.now() - start;
-console.log(nums);
-console.log(`completed in ${start} nanoseconds...`);
-*/
 for(let i = 100; i <= 1000; i+= 100) {
-	size = i;
-	testSort(10);
+	testSort(10,i);
 }
